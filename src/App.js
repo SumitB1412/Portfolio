5c725fb3e8de491a91ade746268bd3b33bde22cc
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-05-27 00:36:43 +0530

    working on sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import './App.css'
+import SimpleSidebar from './components/SideBar'
 
 function App() {
   return (
     <div className="App">
-      
+      <SimpleSidebar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

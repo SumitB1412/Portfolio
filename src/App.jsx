9ae234036dc610dbb8e5388e89432b9fdcd0e06a
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-05-27 01:10:02 +0530

    setting up app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.scss'
+// import { SideBar } from './components/SideBar/SideBar'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App

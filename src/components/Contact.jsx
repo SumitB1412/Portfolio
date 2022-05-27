f5b72f8b27cb1bb1529ff8c96bf8bbef0aca08de
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-05-27 19:31:15 +0530

    completed portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,49 @@
-// import React from 'react'
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from 'react-icons/hi';
 
-// const Contact = () => {
-//   return (
-//     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-//         <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
-//             <div className='pb-8'>
-//                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-//             </div>
-//             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-//             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-//             <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-//             <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
-//         </form>
-//     </div>
-//   )
-// }
 
-// export default Contact
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      {/* <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'> */}
+      <div className="pb-8">
+        <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+          Contact
+        </p>
+      </div>
+      <div style={{marginTop:"300px",marginLeft:"-130px"}}>
+        <div style={{margin:"20px 0px"}}>
+          <a
+            className="flex  items-center w-full text-gray-300"
+            href="https://www.linkedin.com/in/sumit-beniwal-987595232/"
+          >
+            <FaLinkedin style={{marginRight:"10px"}} size={30} /> https://www.linkedin.com/in/sumit-beniwal-987595232/
+          </a>
+
+        </div>
+        <div style={{margin:"20px 0px"}}>
+          <a
+            className="flex  items-center w-full text-gray-300"
+            href="https://github.com/SumitB1412"
+          >
+             <FaGithub style={{marginRight:"10px"}} size={30} /> https://github.com/SumitB1412
+          </a>
+        </div>
+        <div style={{margin:"20px 0px"}}>
+        <a
+              className='flex  items-center w-full text-gray-300'
+              href='mailto:beniwalsumit1412@gmail.com'
+            >
+               <HiOutlineMail style={{marginRight:"10px"}} size={32} /> beniwalsumit1412@gmail.com
+            </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;

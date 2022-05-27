f74145feeb8d2022a47b1abae50bef7c3d026e3d
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-05-27 11:01:57 +0530

    starting from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.scss'
-import Layout from './components/Layout/Layout'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />} />
-      </Routes>
+      
     </div>
   )
 }

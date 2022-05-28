b7065b9049f1f4d5a923cc72539a9593fd581cd5
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-05-28 21:23:04 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,13 +20,17 @@ const Contact = () => {
     >
       {/* <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'> */}
       <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+        <p
+          style={{ background: bg, color: text }}
+          className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300"
+        >
           Contact
         </p>
       </div>
       <div style={{ marginTop: "300px", marginLeft: "-130px" }}>
         <div style={{ margin: "20px 0px" }}>
           <a
+            style={{ background: bg, color: text }}
             className="flex  items-center w-full text-gray-300"
             href="https://www.linkedin.com/in/sumit-beniwal-987595232/"
           >
@@ -36,6 +40,7 @@ const Contact = () => {
         </div>
         <div style={{ margin: "20px 0px" }}>
           <a
+            style={{ background: bg, color: text }}
             className="flex  items-center w-full text-gray-300"
             href="https://github.com/SumitB1412"
           >
@@ -45,6 +50,7 @@ const Contact = () => {
         </div>
         <div style={{ margin: "20px 0px" }}>
           <a
+            style={{ background: bg, color: text }}
             className="flex  items-center w-full text-gray-300"
             href="mailto:beniwalsumit1412@gmail.com"
           >
